2c5df77a8f326137b7e5b9e9cfaad8ba15776b7b
lijieming <8977355+minglicode@user.noreply.gitee.com>
2024-09-26 22:08:01 +0800

    fix: 去掉workspaceid

--- igdp-common/src/main/java/com/inspur/common/core/domain/AjaxResult.java
+++ igdp-common/src/main/java/com/inspur/common/core/domain/AjaxResult.java
@@ -1,46 +1,47 @@
 package com.inspur.common.core.domain;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Objects;
-
 import com.inspur.common.constant.HttpStatus;
 import com.inspur.common.utils.StringUtils;
-import org.apache.poi.ss.formula.functions.T;
+
+import java.util.HashMap;
+import java.util.Objects;
 
 /**
  * 操作消息提醒
  *
  * @author liyunlong
  */
-public class AjaxResult extends HashMap<String, Object>
-{
+public class AjaxResult extends HashMap<String, Object> {
     private static final long serialVersionUID = 1L;
 
-    /** 状态码 */
+    /**
+     * 状态码
+     */
     public static final String CODE_TAG = "code";
 
-    /** 返回内容 */
+    /**
+     * 返回内容
+     */
     public static final String MSG_TAG = "msg";
 
-    /** 数据对象 */
+    /**
+     * 数据对象
+     */
     public static final String DATA_TAG = "data";
 
     /**
      * 初始化一个新创建的 AjaxResult 对象，使其表示一个空消息。
      */
-    public AjaxResult()
-    {
+    public AjaxResult() {
     }
 
     /**
      * 初始化一个新创建的 AjaxResult 对象
      *
      * @param code 状态码
-     * @param msg 返回内容
+     * @param msg  返回内容
      */
-    public AjaxResult(int code, String msg)
-    {
+    public AjaxResult(int code, String msg) {
         super.put(CODE_TAG, code);
         super.put(MSG_TAG, msg);
     }
@@ -49,34 +50,29 @@ public class AjaxResult extends HashMap<String, Object>
      * 初始化一个新创建的 AjaxResult 对象
      *
      * @param code 状态码
-     * @param msg 返回内容
+     * @param msg  返回内容
      * @param data 数据对象
      */
-    public AjaxResult(int code, String msg, Object data)
-    {
+    public AjaxResult(int code, String msg, Object data) {
         super.put(CODE_TAG, code);
         super.put(MSG_TAG, msg);
-        if (StringUtils.isNotNull(data))
-        {
+        if (StringUtils.isNotNull(data)) {
             super.put(DATA_TAG, data);
         }
     }
-    public AjaxResult(int status, String msg, String code)
-    {
-        super.put("status", status);
-        super.put("message", msg);
-        if (StringUtils.isNotNull(code))
-        {
-            super.put(DATA_TAG, code);
-        }
+
+    public AjaxResult(int status, String msg, String code) {
+        super.put("status" , status);
+        super.put("message" , msg);
+        super.put("code" , code);
     }
+
     /**
      * 返回成功消息
      *
      * @return 成功消息
      */
-    public static AjaxResult success()
-    {
+    public static AjaxResult success() {
         return AjaxResult.success("操作成功");
     }
 
@@ -85,9 +81,8 @@ public class AjaxResult extends HashMap<String, Object>
      *
      * @return 成功消息
      */
-    public static AjaxResult success(Object data)
-    {
-        return AjaxResult.success("操作成功", data);
+    public static AjaxResult success(Object data) {
+        return AjaxResult.success("操作成功" , data);
     }
 
     /**
@@ -96,20 +91,18 @@ public class AjaxResult extends HashMap<String, Object>
      * @param msg 返回内容
      * @return 成功消息
      */
-    public static AjaxResult success(String msg)
-    {
+    public static AjaxResult success(String msg) {
         return AjaxResult.success(msg, null);
     }
 
     /**
      * 返回成功消息
      *
-     * @param msg 返回内容
+     * @param msg  返回内容
      * @param data 数据对象
      * @return 成功消息
      */
-    public static AjaxResult success(String msg, Object data)
-    {
+    public static AjaxResult success(String msg, Object data) {
         return new AjaxResult(HttpStatus.SUCCESS, msg, data);
     }
 
@@ -120,20 +113,18 @@ public class AjaxResult extends HashMap<String, Object>
      * @param msg 返回内容
      * @return 警告消息
      */
-    public static AjaxResult warn(String msg)
-    {
+    public static AjaxResult warn(String msg) {
         return AjaxResult.warn(msg, null);
     }
 
     /**
      * 返回警告消息
      *
-     * @param msg 返回内容
+     * @param msg  返回内容
      * @param data 数据对象
      * @return 警告消息
      */
-    public static AjaxResult warn(String msg, Object data)
-    {
+    public static AjaxResult warn(String msg, Object data) {
         return new AjaxResult(HttpStatus.WARN, msg, data);
     }
 
@@ -142,8 +133,7 @@ public class AjaxResult extends HashMap<String, Object>
      *
      * @return 错误消息
      */
-    public static AjaxResult error()
-    {
+    public static AjaxResult error() {
         return AjaxResult.error("操作失败");
     }
 
@@ -153,20 +143,18 @@ public class AjaxResult extends HashMap<String, Object>
      * @param msg 返回内容
      * @return 错误消息
      */
-    public static AjaxResult error(String msg)
-    {
+    public static AjaxResult error(String msg) {
         return AjaxResult.error(msg, null);
     }
 
     /**
      * 返回错误消息
      *
-     * @param msg 返回内容
+     * @param msg  返回内容
      * @param data 数据对象
      * @return 错误消息
      */
-    public static AjaxResult error(String msg, Object data)
-    {
+    public static AjaxResult error(String msg, Object data) {
         return new AjaxResult(HttpStatus.ERROR, msg, data);
     }
 
@@ -174,11 +162,10 @@ public class AjaxResult extends HashMap<String, Object>
      * 返回错误消息
      *
      * @param code 状态码
-     * @param msg 返回内容
+     * @param msg  返回内容
      * @return 错误消息
      */
-    public static AjaxResult error(int code, String msg)
-    {
+    public static AjaxResult error(int code, String msg) {
         return new AjaxResult(code, msg, null);
     }
 
@@ -187,8 +174,7 @@ public class AjaxResult extends HashMap<String, Object>
      *
      * @return 结果
      */
-    public boolean isSuccess()
-    {
+    public boolean isSuccess() {
         return Objects.equals(HttpStatus.SUCCESS, this.get(CODE_TAG));
     }
 
@@ -197,8 +183,7 @@ public class AjaxResult extends HashMap<String, Object>
      *
      * @return 结果
      */
-    public boolean isWarn()
-    {
+    public boolean isWarn() {
         return Objects.equals(HttpStatus.WARN, this.get(CODE_TAG));
     }
 
@@ -207,24 +192,23 @@ public class AjaxResult extends HashMap<String, Object>
      *
      * @return 结果
      */
-    public boolean isError()
-    {
+    public boolean isError() {
         return Objects.equals(HttpStatus.ERROR, this.get(CODE_TAG));
     }
-    public AjaxResult badRequest(String code, String msg)
-    {
+
+    public AjaxResult badRequest(String code, String msg) {
         return new AjaxResult(HttpStatus.BAD_REQUEST, code, msg);
     }
+
     /**
      * 方便链式调用
      *
-     * @param key 键
+     * @param key   键
      * @param value 值
      * @return 数据对象
      */
     @Override
-    public AjaxResult put(String key, Object value)
-    {
+    public AjaxResult put(String key, Object value) {
         super.put(key, value);
         return this;
     }
