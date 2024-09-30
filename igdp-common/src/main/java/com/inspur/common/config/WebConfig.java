6e0c21e1ad80c99ea2592dda345f57e613ae995a
lijieming <8977355+minglicode@user.noreply.gitee.com>
2024-09-30 09:22:19 +0800

    fix: 修改代理大模型事件流接口 data:后增加空格

---
+++ igdp-common/src/main/java/com/inspur/common/config/WebConfig.java
@@ -0,0 +1,14 @@
+package com.inspur.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.AsyncSupportConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void configureAsyncSupport(AsyncSupportConfigurer configurer) {
+        configurer.setDefaultTimeout(1000000); // 设置超时时间为100秒
+    }
+}
