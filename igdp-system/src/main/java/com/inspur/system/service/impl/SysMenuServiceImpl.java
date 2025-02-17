2e51f71bbf3adb7cd71e00cd79afc55b799a9d18
lijieming <8977355+minglicode@user.noreply.gitee.com>
2025-02-17 16:19:21 +0800

    feat:菜单接口 不配置ip端口能返回地址link

--- igdp-system/src/main/java/com/inspur/system/service/impl/SysMenuServiceImpl.java
+++ igdp-system/src/main/java/com/inspur/system/service/impl/SysMenuServiceImpl.java
@@ -147,6 +147,8 @@ public class SysMenuServiceImpl extends MPJBaseServiceImpl<SysMenuMapper, SysMen
                             SysApp app = appMap.get(menu.getAppId());
                             if (null != app && StrUtil.isNotEmpty(app.getAppServer())) {
                                 menu.setLink(app.getAppServer() + menu.getLink());
+                            }else if(menu.getLink().startsWith("/")){
+                                menu.setLink( menu.getLink());
                             }
                         }
                     }
