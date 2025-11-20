c36cf301210c3161b4ada1ff6a7eef14f619fe40
liuhao17 <liuhao17@inspuer.com>
2025-11-20 10:41:31 +0800

    feat:线上nacos;修复角色赋值问题

--- igdp-admin/src/main/java/com/inspur/web/controller/system/SysLoginController.java
+++ igdp-admin/src/main/java/com/inspur/web/controller/system/SysLoginController.java
@@ -1,5 +1,6 @@
 package com.inspur.web.controller.system;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
@@ -115,7 +116,7 @@ public class SysLoginController {
         AjaxResult ajax = AjaxResult.success();
         // 如果是有 admin 权限，则返回所有权限字符
         if (!CollectionUtils.isEmpty(roles) && roles.contains(Constants.SUPER_ADMIN)) {
-            permissions.clear();
+            permissions = new HashSet<>();
             permissions.add(Constants.ALL_PERMISSION);
         }
         ajax.put("user", user);
