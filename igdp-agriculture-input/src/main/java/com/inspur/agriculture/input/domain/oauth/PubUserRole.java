38c8c6ce7229004b34bfd19e7db75e3915fab5e5
Yuanxuan88 <renyuxuan@inspur.com>
2025-11-25 15:04:53 +0800

    feat:供应商审批流程审批成功后增加用户角色赋权

---
+++ igdp-agriculture-input/src/main/java/com/inspur/agriculture/input/domain/oauth/PubUserRole.java
@@ -0,0 +1,27 @@
+package com.inspur.agriculture.input.domain.oauth;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * 用户角色关联对象 pub_user_role (oauth2_bsp模式)
+ *
+ * @author igdp
+ */
+@Data
+@TableName("oauth2_bsp.pub_user_role")
+public class PubUserRole implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    /** 用户ID */
+    private String userCode;
+
+    /** 角色ID */
+    private String roleCode;
+
+    private String appCode;
+}
---
+++ igdp-agriculture-input/src/main/java/com/inspur/agriculture/input/mapper/oauth/PubUserRoleMapper.java
@@ -0,0 +1,12 @@
+package com.inspur.agriculture.input.mapper.oauth;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.inspur.agriculture.input.domain.oauth.PubUserRole;
+
+/**
+ * 用户角色关联Mapper接口 (oauth2_bsp模式)
+ *
+ * @author igdp
+ */
+public interface PubUserRoleMapper extends BaseMapper<PubUserRole> {
+}
