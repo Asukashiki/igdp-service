25d1773dd8638c0d43164a4553b7dc31b5ca440c
石擎天 <sparkmouse@foxmail.com@qq.com>
2025-11-28 11:13:04 +0800

    1

--- igdp-seed/src/main/java/com/inspur/seed/domain/SeedPromotionInfo.java
+++ igdp-seed/src/main/java/com/inspur/seed/domain/SeedPromotionInfo.java
@@ -70,4 +70,27 @@ public class SeedPromotionInfo extends BaseEntity {
      * 访问次数
      */
     private Integer visitCount;
+
+
+    /**
+     * 创建者
+     */
+    private String createBy;
+
+    /**
+     * 创建时间
+     */
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime createTime;
+
+    /**
+     * 更新时间
+     */
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime updateTime;
+
+    /**
+     * 更新者
+     */
+    private String updateBy;
 }
