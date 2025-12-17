2eb5e0f93e63e11612e60b724c1322fe166e11ec
liuhao17 <liuhao17@inspuer.com>
2025-12-17 14:56:50 +0800

    fix :  种子生产数据 添加实验列表

--- igdp-seed/src/main/java/com/inspur/seed/vo/breed/BreedSeedProduceVO.java
+++ igdp-seed/src/main/java/com/inspur/seed/vo/breed/BreedSeedProduceVO.java
@@ -45,6 +45,16 @@ public class BreedSeedProduceVO implements Serializable {
     private String varietyName;
 
     /**
+     * 实验批次ID
+     */
+    private String trialId;
+
+    /**
+     * 实验批次名称(自动带出)
+     */
+    private String trialName;
+
+    /**
      * 作物类型
      */
     private String cropType;
