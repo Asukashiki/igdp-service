32c6fe45cde2830ba6369b92054af4db41b2a952
liuhao17 <liuhao17@inspuer.com>
2025-12-24 16:27:49 +0800

    fix: 调整用户中心注册接口

--- igdp-seed/src/main/java/com/inspur/seed/service/registration/impl/BreedingOrgRegistrationServiceImpl.java
+++ igdp-seed/src/main/java/com/inspur/seed/service/registration/impl/BreedingOrgRegistrationServiceImpl.java
@@ -58,7 +58,7 @@ public class BreedingOrgRegistrationServiceImpl extends ServiceImpl<BreedingOrgR
     @Autowired
     private BreedingAuditLogMapper breedingAuditLogMapper;
 
-    @Value("${user.center.register.url:http://172.26.100.103:9403/rbac/user/register}")
+    @Value("${bsp.center.register.url:http://172.26.100.103:9403/rbac/user/register}")
     private String userCenterRegisterUrl;
 
     private RestTemplate restTemplate;
