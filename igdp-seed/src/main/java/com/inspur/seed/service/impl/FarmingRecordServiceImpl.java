318e7302670cc24c4bb1a563816196a0100ebe88
石擎天 <sparkmouse@foxmail.com>
2025-12-24 22:41:38 +0800

    fix:修改"投入品流通管理"模块的BUG

--- igdp-seed/src/main/java/com/inspur/seed/service/impl/FarmingRecordServiceImpl.java
+++ igdp-seed/src/main/java/com/inspur/seed/service/impl/FarmingRecordServiceImpl.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -45,7 +46,11 @@ public class FarmingRecordServiceImpl implements IFarmingRecordService {
 
         // 设置创建信息（写入用户名，便于前端显示 Creator）
         farmingRecord.setCreateTime(LocalDateTime.now());
+        farmingRecord.setUpdateTime(LocalDateTime.now());
+        farmingRecord.setAuditTime(new Date());
         farmingRecord.setCreateBy(SecurityUtils.getUsername());
+        farmingRecord.setUpdateBy(SecurityUtils.getUsername());
+        farmingRecord.setAuditBy(SecurityUtils.getUsername());
         farmingRecord.setWorkflowStatus("S0");
 
         farmingRecordMapper.insert(farmingRecord);
